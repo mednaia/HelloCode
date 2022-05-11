aac8fa9731f4c3db188b2dcfe0ab10aaec89385d
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-11 22:39:05 +0300

    example008 intro method

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+int b = new Random().Next(10, 99);
+Console.WriteLine(b);
+int c = new Random().Next(10, 99);
+Console.WriteLine(c);
+int d = new Random().Next(10, 99);
+Console.WriteLine(d);
+int e = new Random().Next(10, 99);
+Console.WriteLine(e);
+int f = new Random().Next(10, 99);
+Console.WriteLine(f);
+int g = new Random().Next(10, 99);
+Console.WriteLine(g);
+int o = new Random().Next(10, 99);
+Console.WriteLine(o);
+int p = new Random().Next(10, 99);
+Console.WriteLine(p);
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+if (f>max) max = f;
+if (g>max) max = g;
+if (o>max) max = o;
+if (p>max) max = p;
+
+Console.Write("max = ");
+Console.WriteLine(max);
