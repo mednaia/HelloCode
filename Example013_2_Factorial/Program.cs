340fdeaecfdfe3d1757268e45591d3bf20260f32
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-27 23:05:24 +0300

    example 013 factorial

---
+++ Example013_2_Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿// Factorial
+
+//int Factorial (int n)
+//{
+    //1! = 1
+    //0! = 1
+//    if(n == 1) return 1;
+//    else return n * Factorial(n-1);
+//}
+
+//Console.WriteLine(Factorial(5));
+
+//for (int i = 1; i < 40; i++)
+//{    
+//    Console.WriteLine($"{i}! = {Factorial(i)}");
+//}
+
+double Factorial (int n)
+{
+    //1! = 1
+    //0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{    
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
