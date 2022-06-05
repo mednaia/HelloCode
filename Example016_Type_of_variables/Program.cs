869f9915b5f0c9752c68397021907edc5f4ab838
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-05 15:42:08 +0300

    Type of variables

---
+++ Example016_Type_of_variables/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+// int a = 12;
+// var a = 12;
+// Console.WriteLine(a.GetType().Name);
+
+var data = new int[]{1,2,3,4}
+        .Where(e=> e>0)
+        .Select(e=> new{q=e,w=e+1});
+
+System.Console.WriteLine(data.GetType().Name);
